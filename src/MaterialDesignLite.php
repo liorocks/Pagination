b7c863d59b7362aff4b3501ad9fcc85ff0f1f195
bonsi <https://github.com/bonsi>
2016-07-08 22:06:54 +0200

    isolate elements in mdl-grid & inline css

--- src/MaterialDesignLite.php
+++ src/MaterialDesignLite.php
@@ -4,24 +4,31 @@ class MaterialDesignLite extends Pagination {
 
     /**
      * Pagination wrapper HTML.
-     *
+     * Use class 'Pagination__container' for extra container styling, 'Pagination__nav' for extra nav styling
      * @var string
      */
-    protected $paginationWrapper = '<nav class="pagination-nav mdl-color-text--grey-50 mdl-cell mdl-cell--12-col">%s %s %s</nav>';
+    protected $paginationWrapper =
+        '<div class="mdl-grid Pagination__container" style="display: flex;justify-content: center;">' .
+        '<nav style="display: flex;justify-content: center;" class="mdl-cell mdl-cell--12-col Pagination__nav">%s %s %s</nav>' .
+        '</div>';
 
     /**
      * Available page wrapper HTML.
-     *
+     * Use class 'Pagination__available-page-button' for extra available page button styling
      * @var string
      */
-    protected $availablePageWrapper = '<a href="%s"><button class="mdl-button mdl-js-button mdl-button--colored">%s</button></a>';
+    protected $availablePageWrapper =
+        '<a href="%s">' .
+        '<button class="mdl-button mdl-js-button mdl-button--colored Pagination__available-page-button">%s</button>' .
+        '</a>';
 
     /**
      * Get active page wrapper HTML.
-     *
+     * Use class 'Pagination__active-page-button' for extra active page button styling
      * @var string
      */
-    protected $activePageWrapper = '<button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" disabled>%s</button>';
+    protected $activePageWrapper =
+        '<button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored Pagination__active-page-button" disabled>%s</button>';
 
     /**
      * Get disabled page wrapper HTML.
