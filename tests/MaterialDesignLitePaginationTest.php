b1d52a655b0c6b2a52534b3192508afe0a523079
bonsi <https://github.com/bonsi>
2016-07-08 22:07:50 +0200

    adding MaterialDesignLite tests

---
+++ tests/MaterialDesignLitePaginationTest.php
@@ -0,0 +1,61 @@
+<?php
+use Illuminate\Pagination\LengthAwarePaginator;
+use Landish\Pagination\MaterialDesignLite as MaterialDesignLitePresenter;
+
+class MaterialDesignLitePaginationTest extends PHPUnit_Framework_TestCase {
+
+    protected $array = [];
+    protected $dir;
+
+    public function __construct() {
+        for ($i = 1; $i <= 13; $i++) {
+            $this->array[$i] = 'item' . $i;
+        }
+        $this->dir = __DIR__ . '/fixtures/mdl/';
+    }
+
+    public function testMaterializePresenterCanGenerateLinksForSlider() {
+        $p = new LengthAwarePaginator($this->array, count($this->array), 1, 7);
+        $presenter = new MaterialDesignLitePresenter($p);
+        $this->assertEquals(
+            trim(file_get_contents($this->dir . 'slider.html')),
+            $presenter->render()
+        );
+    }
+
+    public function testMaterializePresenterCanGenerateLinksForTooCloseToBegining() {
+        $p = new LengthAwarePaginator($this->array, count($this->array), 1, 2);
+        $presenter = new MaterialDesignLitePresenter($p);
+        $this->assertEquals(
+            trim(file_get_contents($this->dir . 'beginning.html')),
+            $presenter->render()
+        );
+    }
+
+    public function testMaterializePresenterCanGenerateLinksForTooCloseToEnding() {
+        $p = new LengthAwarePaginator($this->array, count($this->array), 1, 12);
+        $presenter = new MaterialDesignLitePresenter($p);
+        $this->assertEquals(
+            trim(file_get_contents($this->dir . 'ending.html')),
+            $presenter->render()
+        );
+    }
+
+    public function testMaterializePresenterCanGenerateLinksForLastPage() {
+        $p = new LengthAwarePaginator($this->array, count($this->array), 1, 13);
+        $presenter = new MaterialDesignLitePresenter($p);
+        $this->assertEquals(
+            trim(file_get_contents($this->dir . 'last_page.html')),
+            $presenter->render()
+        );
+    }
+
+    public function testMaterializePresenterCanGenerateLinksForFirstPage() {
+        $p = new LengthAwarePaginator($this->array, count($this->array), 1, 1);
+        $presenter = new MaterialDesignLitePresenter($p);
+        $this->assertEquals(
+            trim(file_get_contents($this->dir . 'first_page.html')),
+            $presenter->render()
+        );
+    }
+}
